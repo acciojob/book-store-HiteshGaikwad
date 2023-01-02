8bb66dcd9e6188600ac391fafe28a7eae0b929e1
Hitesh <gaikwadhitesh03@gmail.com>
2023-01-02 21:20:34 +0530

    e chnged

--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -34,7 +34,7 @@ public class BookRepository {
     // save book to the database
     public Book save(Book book){
         book.setId(id);
-        bookList.add(id,book);
+        getBookList().add(id,book);
         id++;
         return book;
     }
